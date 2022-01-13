058dd7a87198ce2f8fb8aeccfd5bc633d3384f53
Igor Deon <igordeon709@gmail.com>
2022-01-13 04:21:22 -0300

    Form layout

--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -1,6 +1,43 @@
+import './styles.css'
+
+const movie = {
+    id: 1,
+    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5
+};
+
 function Form() {
     return (
-        <h1>Form</h1>
+        <div className="project-form-container">
+            <img src={movie.image} alt={movie.title} className='project-form-img' />
+            <div className="project-formbottom">
+                <h1>{movie.title}</h1>
+                <form className="project-formbottom-form">
+                    <div className="project-formbottom-formgroup">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="project-formbottom-formgroup-control" id="email" />
+                    </div>
+                    <div className="project-formbottom-formgroup">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="project-formbottom-formgroup-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="project-formbottom-button-container">
+                        <button className="project-formbottom-button" id="save">Salvar</button>
+                    </div>
+                </form>
+                <div className="project-formbottom-button-container">
+                    <button className="project-formbottom-button" id="cancel">Cancelar</button>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Form;
