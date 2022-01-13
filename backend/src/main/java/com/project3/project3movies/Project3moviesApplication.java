3f6e0678c500df1b64e60ec91f02da01ec18c0af
Igor Deon <igordeon709@gmail.com>
2022-01-13 01:56:57 -0300

    Project created

---
+++ backend/src/main/java/com/project3/project3movies/Project3moviesApplication.java
@@ -0,0 +1,13 @@
+package com.project3.project3movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3moviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3moviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project3/project3movies/Project3moviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project3.project3movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3moviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
