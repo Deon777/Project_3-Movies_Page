12db13d82c768287d4f47b6b47b587a715af83b7
Igor Deon <igordeon709@gmail.com>
2022-01-16 19:56:50 -0300

    Pagination

--- frontend/src/Components/Pagination/index.tsx
+++ frontend/src/Components/Pagination/index.tsx
@@ -1,18 +1,26 @@
 import './styles.css';
 import { ReactComponent as Arrow } from 'Assets/img/arrow.svg';
+import { MoviePage } from 'Types/movie';
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+function Pagination( { page, onChange } : Props ) {
     return (
         <div className='project-pagination-container'>
             <div className='project-pagination-box'>
-                <button className='project-pagination-bttarrow' id='left' disabled={true}>
-                    <Arrow />
+                <button className='project-pagination-bttarrow' id='left' 
+                    disabled={page.first} onClick={() => onChange(page.number-1)}>
+                        <Arrow />
                 </button>
-                <p className='project-pagination-numberpage'>{1} de {3}</p>
-                <button className='project-pagination-bttarrow' id='right' disabled={false}>
-                    <Arrow />
+                <p className='project-pagination-numberpage'>{page.number + 1} de {page.totalPages}</p>
+                <button className='project-pagination-bttarrow' id='right' 
+                    disabled={page.last} onClick={() => onChange(page.number+1)}>
+                        <Arrow />
                 </button>
-            </div>
+            </div>  
         </div>
     );
 }
--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -2,28 +2,41 @@ import axios from 'axios';
 import MovieCard from 'Components/MovieCard';
 import Pagination from 'Components/Pagination';
 import { useEffect, useState } from 'react';
-import { Movie, MoviePage } from 'Types/movie';
+import { MoviePage } from 'Types/movie';
 import { BASE_URL } from 'Utils/requests';
 import './styles.css';
 
 function Listing() {
 
-    const [currentPage, setCurrentPage] = useState(2);
-    const [movies, setMovies] = useState<Movie[]>([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [moviesPage, setMoviesPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 0,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+    
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage);
+    }
 
     useEffect(() => {
         axios.get(`${BASE_URL}/movies?size=12&sort=title&page=${currentPage}`).then(response => {
-            let responseMovies = response.data as MoviePage;
-            setMovies(responseMovies.content);
+            setMoviesPage(response.data);
         });
     }, [currentPage]);
 
     return (
         <div>
-            <Pagination />
+            <Pagination page={moviesPage} onChange={handlePageChange}/>
             <div className='project-listing-container'>
                 <div className='row'>
-                    {movies.map(movie => {
+                    {moviesPage.content.map(movie => {
                         return (
                             <div key={movie.id} className='col-sm-6 col-lg-4 col-xl-3 mb-4'>
                                 <MovieCard movie={movie}/>
